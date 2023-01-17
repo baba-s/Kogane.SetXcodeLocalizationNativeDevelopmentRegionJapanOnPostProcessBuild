362e84dccb95d6e2f39d1d23e5493c08e4c77173
baba-s <shotababa1206@gmail.com>
2023-01-17 19:38:06 +0900

    add

---
+++ Editor/SetXcodeLocalizationNativeDevelopmentRegionJapanOnPostProcessBuild.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Kogane.Internal
+{
+    /// <summary>
+    /// iOS ビルド完了時に Xcode プロジェクトの Localization native development region に Japan を設定するエディタ拡張
+    /// </summary>
+    internal static class SetXcodeLocalizationNativeDevelopmentRegionJapanOnPostProcessBuild
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// ビルド完了時に呼び出されます
+        /// </summary>
+        [PostProcessBuild]
+        private static void OnPostProcessBuild
+        (
+            BuildTarget buildTarget,
+            string      pathToBuiltProject
+        )
+        {
+            if ( buildTarget != BuildTarget.iOS ) return;
+
+            var path  = $"{pathToBuiltProject}/Info.plist";
+            var plist = new PlistDocument();
+
+            plist.ReadFromFile( path );
+            var root = plist.root;
+            root.SetString( "CFBundleDevelopmentRegion", "ja_JP" );
+            plist.WriteToFile( path );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SetXcodeLocalizationNativeDevelopmentRegionJapanOnPostProcessBuild
+# Kogane Set Xcode Localization Native Development Region Japan On Post Process Build
+
+iOS ビルド完了時に Xcode プロジェクトの Localization native development region に Japan を設定するエディタ拡張
